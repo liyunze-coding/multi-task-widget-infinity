a7345a7ad5474be32fbfe10f7b674c52c3045ac6
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2024-05-08 21:57:21 +0800

    updated task count margin right

--- configs.js
+++ configs.js
@@ -48,7 +48,7 @@ const configs = (function () {
 		headerFontWeight: "bold", // normal or bold or number
 		headerFontColor: "#fff", // hex or name
 
-		taskCountMarginRight: "175px", // px
+		taskCountMarginRight: "0px", // px
 
 		// body
 		bodyBackgroundColor: "#fff", // hex only
